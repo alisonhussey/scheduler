a38ed451b51416f01e55354af70f9cc8e057a7e8
alisonhussey <alisonhussey@gmail.com>
2021-01-21 21:49:23 -0500

    created getInterview helper to get interview data based on interviewer id

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,14 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-
+//Gets all appointment ids that match a given day
 export function getAppointmentsForDay(state, day) {
-  
-  const filteredDays = state.days.filter(eachDay => eachDay.name === day)
+  const filteredDays = state.days.filter((eachDay) => eachDay.name === day);
   if (filteredDays.length === 0) {
     return [];
   }
 
-  const appointments = filteredDays[0].appointments.map(appointmentId => state.appointments[appointmentId])
-  return appointments
+  const appointments = filteredDays[0].appointments.map(
+    (appointmentId) => state.appointments[appointmentId]
+  );
+  return appointments;
 }
-   
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+  
+  if (state.interviewers.id === interview.interviewer.id) {
+    return {
+      student: interview.student,
+      interviewer: {
+        id: interview.interviewer,
+        name: state.interviewers[interview.interviewer].name,
+        avatar: state.interviewers[interview.interviewer].avatar
+      },
+    };
+  }
+}
