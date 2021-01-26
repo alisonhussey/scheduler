8fbc43bf48db36b92740287399933b5d51005e85
alisonhussey <alisonhussey@gmail.com>
2021-01-25 20:33:58 -0500

    Added __tests_ directory and tests for Appointment and Form compontents, refactored form component to pass failing tests

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -10,6 +10,7 @@ import Button from "components/Button";
 export default function Form(props){
   const [name, setName] = useState(props.name || "" );
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = function() {
     setName("");
@@ -21,7 +22,16 @@ export default function Form(props){
     props.onCancel();
   };
 
-  const save = function() {
+  // const save = function() {
+  //   props.onSave(name, interviewer);
+  // }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("")
     props.onSave(name, interviewer);
   }
 
@@ -36,8 +46,9 @@ export default function Form(props){
             placeholder="Enter Student Name"
             value={name}
             onChange={event => setName(event.target.value)}
-
+            data-testid="student-name-input"
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList 
           interviewers={props.interviewers} 
@@ -47,7 +58,7 @@ export default function Form(props){
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
